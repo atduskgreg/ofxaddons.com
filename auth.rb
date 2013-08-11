799cca2447a05ab78f499581f1901f5ab5a78272
James George <james@jamesgeorge.org>
2013-08-11 11:29:03 +0900

    deleting accidentally added auth.rb

--- auth.rb
+++
@@ -1,5 +0,0 @@
-#secret client stuff
-#client_id = "11bc079c5c5e5ec9d45b"
-#client_secret = "bb1ebc21adf837a5971136a1480497ba7c3559c4"
-
-$auth_params = "client_id=11bc079c5c5e5ec9d45b&client_secret=bb1ebc21adf837a5971136a1480497ba7c3559c4"
