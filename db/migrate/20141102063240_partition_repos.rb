b93d062f389f3f97052bb741fef30e16f95ff7eb
James Hughes <james@virtualjames.com>
2015-01-25 15:29:18 -0800

    moving category creation to rake task

--- db/migrate/20141102063240_partition_repos.rb
+++ db/migrate/20141102063240_partition_repos.rb
@@ -2,18 +2,6 @@ class PartitionRepos < ActiveRecord::Migration
 
   def up
     add_column :repos, :type, :string, default: "Unsorted", null: false
-    Repo.transaction do
-      Repo.all.each do |r|
-        case
-        when r.deleted       then r.type = "Deleted";  r.save!
-        when r.not_addon     then r.type = "NonAddon"; r.save!
-        when !!r.category_id
-          addon = r.becomes!(Addon)
-          addon.categories << Category.find(addon.category_id)
-          addon.save!
-        end
-      end
-    end
   end
 
   def down
