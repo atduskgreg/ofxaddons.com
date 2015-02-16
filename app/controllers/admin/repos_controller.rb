db83edfa039cb107faca29fdcbbd48977bae32bc
James Hughes <james@virtualjames.com>
2015-02-16 02:50:23 -0800

    fragement cache expiry for categories

--- app/controllers/admin/repos_controller.rb
+++ app/controllers/admin/repos_controller.rb
@@ -31,10 +31,18 @@ class Admin::ReposController < Admin::ApplicationController
   def update
     raise "ajax only!" unless request.xhr?
 
-    repo = Repo.find(repo_id)
+    repo = Repo.includes(:categories).find(repo_id)
     repo.type = repo_type
+    unaffected_category_ids = repo.category_ids & repo_category_ids
+    affected_category_ids = (repo.category_ids + repo_category_ids - unaffected_category_ids).uniq
     repo.category_ids = repo_category_ids
+
     if repo.save
+      # expire the fragment caches for the affected categories
+      affected_category_ids.each do |category_id|
+        Category.find(category_id).touch
+      end
+
       render(json: {
                status: 200,
                controller: params[:controller],
@@ -59,7 +67,7 @@ class Admin::ReposController < Admin::ApplicationController
   private
 
   def repo_category_ids
-    repo_params[:category_ids]
+    @repo_category_ids ||= repo_params[:category_ids].map(&:to_i) - [0]
   end
 
   def repo_id
