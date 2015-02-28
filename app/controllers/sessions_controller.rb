21dfaab0c18bd8b5bacbd1c26f1ccd4b2d5baf1a
James Hughes <james@virtualjames.com>
2015-02-28 14:59:36 -0800

    added flash messages to the application layout. blocked user login for non-admins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,21 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.login(auth)
-    session[:user_id] = user.id
-    cookies[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+
+    if user.admin?
+      session[:user_id] = user.id
+      cookies[:user_id] = user.id
+      redirect_to root_url, :success => "Signed in!"
+    else
+      user = nil
+      redirect_to root_url, :alert => "Sorry! At the moment there's nothing useful to do with login unless you're an admin. Stay tuned, we'll be adding some user-facing features in the future."
+    end
   end
 
   def destroy
     session[:user_id] = nil
     cookies.delete(:user_id)
-    redirect_to root_url, :notice => "Signed out!"
+    redirect_to root_url, :success => "Signed out!"
   end
 
 end
