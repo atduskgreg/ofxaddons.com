c713183e02bd68fe93a24d29fd9d8c47c4a8405c
James Hughes <james@virtualjames.com>
2015-03-03 00:02:39 -0800

    fixed weird implicit to_s safebuffer madness. DRY'd up the categories index page.

--- app/presenters/category_presenter.rb
+++ app/presenters/category_presenter.rb
@@ -1,13 +1,5 @@
 class CategoryPresenter < Presenter
 
-  # def anchor_link
-  #   h.link_to(object.name, h.categories_path(anchor: object.name.parameterize))
-  # end
-
-  # def anchor_tag
-  #   h.content_tag(:a, "", name: object.name.parameterize)
-  # end
-
   def to_s
     h.link_to(name, h.category_path(object))
   end
