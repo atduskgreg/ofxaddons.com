737bcf1d111b54803e7a1aa33ef89cfc61069f5d
James Hughes <james@virtualjames.com>
2016-03-20 21:08:07 -0500

    attempt to fix up broken static links

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,8 @@ set :stage,          :production
 set :deploy_via,     :remote_cache
 set :deploy_to,      "/home/#{fetch(:user)}/#{fetch(:application)}"
 
-set :linked_files,   %w{config/database.yml, config/secrets.yml}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
